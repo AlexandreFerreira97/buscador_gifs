80f9aad4bcc4596591f12838b47ccd968a2c4dd7
Alexandre <alexandrefaria3012@gmail.com>
2023-07-03 21:33:19 -0300

    Request

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -8,6 +10,21 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  late String _search;
+  int _ofset = 0;
+
+  getGifs() async {
+    http.Response response;
+
+    if(_search == null)
+    {
+      response = await http.get('https://api.giphy.com/v1/gifs/trending?api_key=CnDnxKe3FkrtnwBqjuxsD2b6vJ05xhAc&limit=20&offset=0&rating=g&bundle=messaging_non_clips' as Uri);
+    }else{
+      response = await http.get('https://api.giphy.com/v1/gifs/search?api_key=CnDnxKe3FkrtnwBqjuxsD2b6vJ05xhAc&q=$_search&limit=25&offset=$_ofset&rating=g&lang=en&bundle=messaging_non_clips' as Uri);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
