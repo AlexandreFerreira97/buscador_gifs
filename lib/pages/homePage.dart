b02d758fdf459b07623ed43c12eb84503cd57cc9
Alexandre <alexandrefaria3012@gmail.com>
2023-07-05 17:09:31 -0300

    Update homePage.dart

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -11,24 +11,26 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   late String _search;
   int _ofset = 0;
 
-Future<Map> _getGifs() async {
+  Future<Map> _getGifs() async {
     http.Response response;
 
-    if(_search == null)
-    {
-      response = await http.get('https://api.giphy.com/v1/gifs/trending?api_key=CnDnxKe3FkrtnwBqjuxsD2b6vJ05xhAc&limit=20&offset=0&rating=g&bundle=messaging_non_clips' as Uri);
-    }else{
-      response = await http.get('https://api.giphy.com/v1/gifs/search?api_key=CnDnxKe3FkrtnwBqjuxsD2b6vJ05xhAc&q=$_search&limit=25&offset=$_ofset&rating=g&lang=en&bundle=messaging_non_clips' as Uri);
+    if (_search == null) {
+      response = await http.get(
+          'https://api.giphy.com/v1/gifs/trending?api_key=CnDnxKe3FkrtnwBqjuxsD2b6vJ05xhAc&limit=20&offset=0&rating=g&bundle=messaging_non_clips'
+              as Uri);
+    } else {
+      response = await http.get(
+          'https://api.giphy.com/v1/gifs/search?api_key=CnDnxKe3FkrtnwBqjuxsD2b6vJ05xhAc&q=$_search&limit=25&offset=$_ofset&rating=g&lang=en&bundle=messaging_non_clips'
+              as Uri);
     }
     return json.decode(response.body);
   }
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
 
     _getGifs().then((map) {
@@ -41,27 +43,68 @@ Future<Map> _getGifs() async {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black,
-        title: Image.network('https://developers.giphy.com/branch/master/static/header-logo-0fec0225d189bc0eae27dac3e3770582.gif'),
+        title: Image.network(
+            'https://developers.giphy.com/branch/master/static/header-logo-0fec0225d189bc0eae27dac3e3770582.gif'),
         centerTitle: true,
       ),
       backgroundColor: Colors.black,
-      body:const Column(
+      body: Column(
         children: [
-          Padding(
+          const Padding(
             padding: EdgeInsets.all(10.0),
             child: TextField(
               decoration: InputDecoration(
-                labelText: 'Pesquise aqui!',
-                labelStyle: TextStyle(
-                  color: Colors.white,),
-                  border: OutlineInputBorder()
-              ),
-              style: TextStyle(color: Colors.white,fontSize: 18.0),
+                  labelText: 'Pesquise aqui!',
+                  labelStyle: TextStyle(
+                    color: Colors.white,
+                  ),
+                  border: OutlineInputBorder()),
+              style: TextStyle(color: Colors.white, fontSize: 18.0),
               textAlign: TextAlign.center,
             ),
           ),
+          Expanded(
+            child: FutureBuilder(
+              future: _getGifs(),
+              builder: (context, snapshot) {
+                switch(snapshot.connectionState){
+                  case ConnectionState.waiting:
+                  case ConnectionState.none:
+                    return Container(
+                      width: 200.0,
+                      height: 200.0,
+                      alignment: Alignment.center,
+                      child: const CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                        strokeWidth: 5.0,
+                      ),
+                    );
+                  default:
+                    if(snapshot.hasError) return Container();
+                    else return _createGifTable(context,snapshot);
+                }
+              }
+            ),
+          ),
         ],
-      ) ,
+      ),
     );
   }
+
+  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot){
+      return GridView.builder(
+        padding: const EdgeInsets.all(10.0),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10,
+        ),
+        itemCount: 4,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            //child: Image.network(src) ,
+          );
+        },
+      );
+  }
+
 }
