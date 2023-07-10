ada5eaddd37c6b5d36f2e8afeba2f725f5265111
Alexandre <alexandrefaria3012@gmail.com>
2023-07-10 16:51:23 -0300

    Update homePage.dart

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:async';
 
+import 'package:share/share.dart';
+
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -125,6 +127,9 @@ class _HomeState extends State<Home> {
                 fit:BoxFit.cover,) ,
               onTap: (){
                 Navigator.push(context, MaterialPageRoute(builder: (context) => GifPage(snapshot.data['data'][index])));
+              },
+              onLongPress: (){
+                Share.share(snapshot.data['data'][index]['images']['fixed_height']['url']);
               },
             );
           }else{
